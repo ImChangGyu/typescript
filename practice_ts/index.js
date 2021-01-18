563a239ece0dfb126ff0ac59cacb0568e9e5cf60
jadu20 <audu1217@naver.com>
2021-01-18 19:07:54 +0900

    SecondLearn

--- practice_ts/index.js
+++ practice_ts/index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const names = "ImChangGyu", age = 18, gender = "male";
+const sayHi = (names, age, gender) => {
+    console.log(`hello ${names}, you are ${age}, you are a ${gender}`);
+};
+sayHi(names, age);
 //# sourceMappingURL=index.js.map
--- practice_ts/index.ts
+++ practice_ts/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const names = "ImChangGyu",
+    age = 18,
+    gender = "male";
+
+const sayHi = (names, age, gender?) => {
+    console.log(`hello ${names}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi(names, age);
+
+export {};
